51f10e73b648b70b0482722ad5ba57f386c1b378
quaxsze <service@plum.eu.com>
2020-11-25 17:17:01 +0100

    fix settings

--- udata_gouvfr/settings.py
+++ udata_gouvfr/settings.py
@@ -15,5 +15,5 @@ APIGOUVFR_URL = 'https://api.gouv.fr/api/v1/apis'
 APIGOUVFR_ALLOW_OPENNESS = ['open', 'semi_open']
 
 # Newsletter's subscription page's link
-NEWSLETTER_LINK = 'https://infolettres.etalab.gouv.fr/subscribe/rn7y93le1'
-NEWSLETTER_SUB_MESSAGE = 'Pour ne rien manquer, de l’actualité de data.gouv.fr et de l’open data, inscrivez-vous à notre infolettre.'
+NEWSLETTER_LINK = ''
+NEWSLETTER_SUB_MESSAGE = ''
