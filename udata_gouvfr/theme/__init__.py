038a4e634926b8736e864bbc16c6760c9d4cb630
quaxsze <service@plum.eu.com>
2021-01-25 12:34:46 +0100

    improve wording

--- udata_gouvfr/theme/__init__.py
+++ udata_gouvfr/theme/__init__.py
@@ -46,8 +46,8 @@ gouvfr_menu = nav.Bar('gouvfr_menu', [
     nav.Item(_('Organizations'), 'organizations.list'),
     nav.Item(_('Dashboard'), 'site.dashboard'),
     nav.Item(_('Documentation'), None, url='https://doc.data.gouv.fr', items=[
-        nav.Item(_('Documentation'), None, url='https://doc.data.gouv.fr'),
-        nav.Item(_('Guides'), None, url='https://guides.etalab.gouv.fr'),
+        nav.Item(_('Technical documentation'), None, url='https://doc.data.gouv.fr'),
+        nav.Item(_('Open data guides'), None, url='https://guides.etalab.gouv.fr'),
     ]),
     nav.Item(_('News'), 'posts.list'),
 ])
