38f84724ed94edd2d13ac4418c5f9971cc67fbba
Estelle Maudet <estelle.maudet@data.gouv.fr>
2021-06-24 09:50:39 +0200

    Translate and replace @@ strings

--- theme/js/plugins/i18n.js
+++ theme/js/plugins/i18n.js
@@ -6,6 +6,7 @@ import { createI18n } from "vue-i18n";
 
 //TODO : use a glob when supported in Parcel 2 : https://github.com/parcel-bundler/parcel/issues/4683
 import en from "../locales/en.json";
+import fr from "../locales/fr.json";
 
 const missing = (locale, key) => {
   if (key.startsWith("@@")) return key.slice(2);
@@ -24,5 +25,6 @@ export default i18n = createI18n({
   missing,
   messages: {
     en,
+    fr
   },
 });
