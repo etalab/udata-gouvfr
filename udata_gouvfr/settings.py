bd8cecb7a28ead73f201d871aa9bce6ff959d710
Alexandre Bulté <alexandre@bulte.net>
2021-07-06 12:18:32 +0200

    remove setting

--- udata_gouvfr/settings.py
+++ udata_gouvfr/settings.py
@@ -6,10 +6,6 @@ Default settings for udata-gouvfr
 RESOURCES_SCHEMAGOUVFR_ENABLED = True
 SCHEMA_GOUVFR_VALIDATA_URL = 'https://validata.etalab.studio'
 SCHEMA_CATALOG_URL = 'https://schema.data.gouv.fr/schemas/schemas.json'
-# The permalink to the consolidated IRVE dataset's latest resource
-SCHEMA_GOUVFR_IRVE_STABLE_RESOURCE_URL = (
-    "https://www.data.gouv.fr/fr/datasets/r/50625621-18bd-43cb-8fde-6b8c24bdabb3"
-)
 
 # Frontend banner parameters
 BANNER_ACTIVATED = False
